87046c939f2f1d1f0ad820cb5f2f6d0dbd8ea2ba
Dhruv Shah <dhruvshah38@gmail.com>
2022-09-15 12:02:00 -0400

    app contents

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "f19a6d6145mshd8d88f7d4db35d2p196a20jsnfb6a113e97a0", 
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d40f06b6ce6ceb5026ea2a3240057a65"; // enter your key from openweather API
